fa9549fad64de771331a2b94e85fe0e49529718b
Steve Kane <kanesteven@gmail.com>
2015-07-10 02:55:18 +0100

    added more logic to the program to print rotating text

--- Test.elm
+++ Test.elm
@@ -1,3 +1,24 @@
-import Html exposing (text)
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
+import String
+import Mouse
 
-main = text "Hello, world"
+type alias Model = { text: String, x: Int, y: Int }
+
+stateName = "ILLINOIS"
+model = { text = stateName, x = 0, y = 0 }
+
+update : (Int, Int) -> Model -> Model
+update (x, y) m = 
+  let newText = if m.x > 200 then String.reverse stateName else stateName
+  in
+  { m | x <- x , text <- newText}
+
+
+render : Model -> Graphics.Element.Element
+render model =
+  let content rot = [ rotate (degrees (toFloat rot)) (toForm (show model.text))] in
+  collage 300 300 (content model.x)
+
+main : Signal Element
+main = Signal.map render (Signal.foldp update model Mouse.position)
